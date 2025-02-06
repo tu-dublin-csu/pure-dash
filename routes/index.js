ed0a9a0ad1ed0834f7c1b062569ecf5c1aaba50e
Eoin Kilfeather <eoin.kilfeather@tudublin.ie>
2025-02-06 18:16:39 +0000

    change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PURE Dashboard' });
+  res.render('index', { title: 'PURE Dashboard [beta]' });
 });
 
 module.exports = router;
