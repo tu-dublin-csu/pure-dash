3b61a46fc865fa3fc1d66979992f73d1d78ea033
Eoin Kilfeather <eoin.kilfeather@tudublin.ie>
2025-02-11 20:46:28 +0000

    filter headers and decode

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // Log the request headers
-  console.log('Request Headers:', req.headers);
+  // Log the request header x-ms-client-principal and deserialise from base64
+  if (req.headers['x-ms-client-principal']) {
+    const buff = Buffer.from(req.headers['x-ms-client-principal'], 'base64');
+    const text = buff.toString('ascii');
+    console.log('Request Header x-ms-client-principal:', text);
+  }
+  console.log('Request Header x-ms-client-principal:', req.headers['x-ms-client-principal']);
+  // console.log('Request Headers:', req.headers);
   res.render('index', { title: 'PURE Dashboard', pure_url: process.env.PURE_URL });
 });
 
