813735598ecbcfade2581f57f2aa8c77e6d1ee3f
Eoin Kilfeather <eoin.kilfeather@tudublin.ie>
2025-02-11 21:59:01 +0000

    find fullname

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,22 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var xmscp = null;
-  // Log the request header x-ms-client-principal and deserialise from base64
+  var issuer = null;
+  var user_fullname = null;
+  // deserialise from base64 and log the request header x-ms-client-principal to the console
   if (req.headers['x-ms-client-principal']) {
     const buff = Buffer.from(req.headers['x-ms-client-principal'], 'base64');
     const text = buff.toString('ascii');
     xmscp = JSON.stringify(JSON.parse(text), null, 2)
     //pretty print the decoded string
     console.log('Request Header x-ms-client-principal:\n\n', xmscp);
+    const issClaim = xmscp.claims.find(claim => claim.typ === 'iss');
+    if (issClaim) {
+      user_fullname = issClaim.val;
+    }
+    
   }
-  res.render('index', { title: 'PURE Dashboard', pure_url: process.env.PURE_URL, xmscp: xmscp });
+  res.render('index', { title: 'PURE Dashboard', pure_url: process.env.PURE_URL, user_fullnam: user_fullname });
 });
 
 module.exports = router;
